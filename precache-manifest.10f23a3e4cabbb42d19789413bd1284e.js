930fd5834d8b53db72ff95d82e4de7cfe7a9f00c
Chawla <vikas.chawla@monotype.com>
2020-07-16 04:04:37 +0530

    Updates

---
+++ precache-manifest.10f23a3e4cabbb42d19789413bd1284e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19ffa530ecba89386408bb16f26ffb3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89dea5a0911ec723053e",
+    "url": "/static/css/main.cd78c003.chunk.css"
+  },
+  {
+    "revision": "0daa69a901854e8e2285",
+    "url": "/static/js/2.3610b12c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3610b12c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89dea5a0911ec723053e",
+    "url": "/static/js/main.e288c449.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
